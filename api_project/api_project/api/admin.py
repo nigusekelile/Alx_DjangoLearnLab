941ae3df8057db5ad9c6d90cfe29923bbdeee15b
Niguse Kelile Lema <nigusmov16@gmail.com>
2025-11-22 14:20:47 +0300

    Creating project for api practice

---
+++ api_project/api_project/api/admin.py
@@ -0,0 +1,11 @@
+# api/admin.py
+from django.contrib import admin
+from .models import Book
+
+# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created_at']
+    list_filter = ['author', 'created_at']
+    search_fields = ['title', 'author']
