44cd60e06ab2003871d0473bca80a3860dd231de
Niguse Kelile Lema <nigusmov16@gmail.com>
2025-11-22 15:43:21 +0300

    creating the first api endpoints

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,13 @@
+# api/views.py
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
+class BookList(generics.ListAPIView):
+    """
+    API endpoint that allows books to be viewed.
+    """
+    queryset = Book.objects.all().order_by('-created_at')
+    serializer_class = BookSerializer
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,3 +1,4 @@
+# api_project/urls.py
 """
 URL configuration for api_project project.
 
@@ -14,9 +15,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # Include API URLs
 ]
